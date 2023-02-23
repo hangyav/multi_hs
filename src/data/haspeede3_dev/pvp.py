b43aff823548d1cb383577f0921de8297542643c
Viktor Hangya <hangyav@inf.u-szeged.hu>
2023-02-23 13:56:22 +0100

    soft prompts for haspeede3

--- src/data/haspeede3_dev/pvp.py
+++ src/data/haspeede3_dev/pvp.py
@@ -5,7 +5,8 @@ from src.data.prompting import (
     PVP_DICT,
     DatasetPVPs,
     ManualTemplateFactory,
-    ManualVerbalizerFactory
+    ManualVerbalizerFactory,
+    MixedTemplateFactory,
 )
 
 PVP_DICT['haspeede3_dev'] = {
@@ -13,6 +14,8 @@ PVP_DICT['haspeede3_dev'] = {
         prompt_templates=[
             ManualTemplateFactory('{"placeholder":"text_a"} It was {"mask"}'),
             ManualTemplateFactory('{"placeholder":"text_a"} Era {"mask"}'),
+            MixedTemplateFactory('{"soft": "What type of abusive content does the text have?"} {"placeholder":"text_a"} {"mask"}'),
+            MixedTemplateFactory('{"placeholder":"text_a"} {"soft": "What type of abusive content does the text have?"} {"mask"}'),
         ],
         prompt_verbalizers=[
             ManualVerbalizerFactory({
@@ -27,6 +30,8 @@ PVP_DICT['haspeede3_dev'] = {
         pvps={
             0: (0, 0),
             1: (1, 1),
+            2: (2, 0),
+            3: (3, 0),
         },
     ),
 }
