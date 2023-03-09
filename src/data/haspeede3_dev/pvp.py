1cfa14356a6e33ffbd8a866a51b674b05847a497
Viktor Hangya <hangyav@inf.u-szeged.hu>
2023-03-09 08:34:46 +0100

    new haspeede3 prompts

--- src/data/haspeede3_dev/pvp.py
+++ src/data/haspeede3_dev/pvp.py
@@ -16,6 +16,16 @@ PVP_DICT['haspeede3_dev'] = {
             ManualTemplateFactory('{"placeholder":"text_a"} Era {"mask"}'),
             MixedTemplateFactory('{"soft": "What type of abusive content does the text have?"} {"placeholder":"text_a"} {"mask"}'),
             MixedTemplateFactory('{"placeholder":"text_a"} {"soft": "What type of abusive content does the text have?"} {"mask"}'),
+
+            ManualTemplateFactory('{"placeholder":"text_a"} Is this hate speech? {"mask"}'),
+            ManualTemplateFactory('{"placeholder":"text_a"} Is this either hate speech or abusive? {"mask"}'),
+            ManualTemplateFactory('Is this hate speech? {"placeholder":"text_a"} {"mask"}'),
+            ManualTemplateFactory('Is this either hate speech or abusive? {"placeholder":"text_a"} {"mask"}'),
+            MixedTemplateFactory('{"soft": "Is this either hate speech or abusive?"} {"placeholder":"text_a"} {"mask"}'),
+            MixedTemplateFactory('{"soft": "Is this hate speech?"} {"placeholder":"text_a"} {"mask"}'),
+
+            ManualTemplateFactory('Questo è un discorso di odio? {"placeholder":"text_a"} {"mask"}'),
+            MixedTemplateFactory('{"soft": "Questo è un discorso di odio?"} {"placeholder":"text_a"} {"mask"}'),
         ],
         prompt_verbalizers=[
             ManualVerbalizerFactory({
@@ -26,12 +36,30 @@ PVP_DICT['haspeede3_dev'] = {
                 "noHate": ["neutro"],
                 "Hate": ["odio"],
             }),
+            ManualVerbalizerFactory({
+                "noHate": ["No"],
+                "Hate": ["Yes"],
+            }),
+            ManualVerbalizerFactory({
+                "noHate": ["No"],
+                "Hate": ["Sì"],
+            }),
         ],
         pvps={
             0: (0, 0),
             1: (1, 1),
             2: (2, 0),
             3: (3, 0),
+
+            4: (4, 2),
+            5: (5, 2),
+            6: (6, 2),
+            7: (7, 2),
+            8: (8, 2),
+            9: (9, 2),
+
+            10: (10, 3),
+            11: (11, 3),
         },
     ),
 }
