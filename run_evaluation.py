281086ed6616080c2d53be2a53d583da51f8ef9f
Viktor Hangya <hangyav@inf.u-szeged.hu>
2023-04-28 13:07:35 +0200

    updating evaluator with print functionality

--- run_evaluation.py
+++ run_evaluation.py
@@ -6,7 +6,39 @@ from datasets import load_dataset
 from run_classifier import METRICS, compute_metrics
 
 
-def main(prediction_file, output_file, dataset, config, sep=',', split='test', regression=False):
+def print_default_format(preds, df, output_file):
+    labels = df.features['label'].names
+    with open(output_file, "w") as f:
+        print('index\tprediction', file=f)
+        print(
+            '\n'.join([
+                f'{i}\t{labels[p]}' for i, p in enumerate(preds)
+            ]),
+            file=f
+        )
+
+
+def print_haspeede3_format(preds, df, output_file):
+    labels = df.features['label'].names
+    labels = {i: i for i, _ in enumerate(labels)}
+    with open(output_file, "w") as f:
+        print('anonymized_tweet_id\tlabel', file=f)
+        print(
+            '\n'.join([
+                f'{df[i]["id"]}\t{labels[p]}' for i, p in enumerate(preds)
+            ]),
+            file=f
+        )
+
+
+LABELS_FORMATS = {
+    'default': print_default_format,
+    'haspeede3': print_haspeede3_format,
+        }
+
+
+def main(prediction_file, output_file, dataset, config, sep=',', split='test',
+         regression=False, print_labels=False, format='default'):
     preds = list()
     with open(prediction_file, "r") as f:
         for line in f:
@@ -18,20 +50,25 @@ def main(prediction_file, output_file, dataset, config, sep=',', split='test', r
     df = load_dataset(dataset, config)
     refs = np.array([item['label'] for item in df[split]])
 
-    metrics = compute_metrics(
-        preds,
-        refs,
-        df[split].features['label'].names,
-        bool(regression),
-        METRICS
-    )
+    if not print_labels:
+        metrics = compute_metrics(
+            preds,
+            refs,
+            df[split].features['label'].names,
+            bool(regression),
+            METRICS
+        )
 
-    metrics = dict(map(
-        lambda x: (f'{dataset}-{config}_predict_{x[0]}', x[1].item() if isinstance(x[1], np.generic) else x[1]),
-        metrics.items()
-    ))
-    with open(output_file, "w") as f:
-        json.dump(metrics, f, indent=4, sort_keys=True)
+        metrics = dict(map(
+            lambda x: (f'{dataset}-{config}_predict_{x[0]}', x[1].item() if isinstance(x[1], np.generic) else x[1]),
+            metrics.items()
+        ))
+        with open(output_file, "w") as f:
+            json.dump(metrics, f, indent=4, sort_keys=True)
+    else:
+        preds = np.squeeze(preds) if regression else np.argmax(preds, axis=1)
+
+        LABELS_FORMATS[format](preds, df[split], output_file)
 
 
 if __name__ == "__main__":
@@ -43,6 +80,8 @@ if __name__ == "__main__":
     parser.add_argument("--sep", help="Logit separator", type=str, default=',')
     parser.add_argument("--split", help="Dataset split", type=str, default='test')
     parser.add_argument("--regression", help="Is it a regression task?", type=int, default=0)
+    parser.add_argument("--print_labels", help="Print the predicted labels instead of the metrics", type=int, default=0)
+    parser.add_argument("--format", help="Format for the labels output", type=str, default='default', choices=LABELS_FORMATS.keys())
 
     args = parser.parse_args()
     main(**vars(args))
