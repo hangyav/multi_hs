ac7b31a93cc5a61db3fc4142df7d168f11d7a24c
Viktor Hangya <hangyav@inf.u-szeged.hu>
2023-05-02 10:25:20 +0200

    helper functions for saving hf datasets in tsv

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,13 @@
 import os
 import logging
 import torch
+import importlib
 from transformers.integrations import (
     TensorBoardCallback,
     rewrite_logs,
 )
+
+from src.data.prompting import get_pvp_by_name_and_config
 
 
 logger = logging.getLogger(__name__)
@@ -45,3 +48,43 @@ class TensorBoardLogitsCallback(TensorBoardCallback):
                         "is incorrect so we dropped this attribute."
                     )
             self.tb_writer.flush()
+
+
+def import_dataset(dataset_name, package_path='src.data'):
+    importlib.import_module(
+        f'{package_path}.{dataset_name}.{dataset_name}'
+    ).__file__,
+
+
+def _get_verbs(dataset, pid, import_dataset_module=True):
+    if import_dataset_module:
+        import_dataset(dataset.info.splits['train'].dataset_name)
+
+    pvp = get_pvp_by_name_and_config(
+        dataset.info.splits['train'].dataset_name,
+        dataset.config_name
+    )
+    verbalizer = pvp.prompt_verbalizers[pvp.pvps[pid][1]]
+    res = list()
+    for label in dataset.features['label'].names:
+        verb = verbalizer.verbs[label]
+        assert len(verb) == 1
+        res.append(verb[0])
+    return res
+
+
+def save_hf_dataset(dataset, fout, pid=None):
+    labels = dataset.features["label"].names
+    if pid is not None:
+        labels = _get_verbs(dataset, pid)
+    for item in dataset:
+        print(f"{item['text']}\t{labels[item['label']]}", file=fout)
+
+
+def save_hf_datasets(dataset, dir, pid=None,
+                     splits=['train', 'validation', 'test'],
+                     names=['train.txt', 'valid.txt', 'test.txt']):
+    for split, name in zip(splits, names):
+        df = dataset[split]
+        with open(os.path.join(dir, name), 'w') as fout:
+            save_hf_dataset(df, fout, pid)
